62cf031db651b8df0295e3a5351c96032e197110
Jose Maria Flores Zazo <jmfloreszazo@gmail.com>
2022-09-19 16:25:33 +0200

    Add trusted_function
    Add trusted_function

--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int trusted_call()
+int trusted_function()
 {
     int trusted_value = 987654321;
     wait_keyinput("Enter key to show variables values in memory...");
